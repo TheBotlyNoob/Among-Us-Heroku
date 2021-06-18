f4b488b3cd9f93c7ec9a206b08464be59888c88c
TheBotlyNoob <thebotlynoob@gmail.com>
2021-06-17 23:19:22 -0400

    PUSH

--- opt/proxy.js
+++ opt/proxy.js
@@ -23,14 +23,14 @@ async function claimPlayit(url, type, port, file) {
 
   // Playit Needs Authentication From Discord
   await page.goto('https://discord.com/login?redirect_to=%2Foauth2%2Fauthorize%3Fresponse_type%3Dtoken%26client_id%3D705634226527141919%26redirect_uri%3Dhttps%253A%252F%252Fplayit.gg%252Foauth%252Fdiscord%26scope%3Didentify')
-  await page.waitForSelector('#app-mount > div.app-1q1i1E > div > div > div > form > div > div > div.mainLoginContainer-1ddwnR > div.block-egJnc0.marginTop20-3TxNs6 > div.marginBottom20-32qID7 > div > div.inputWrapper-31_8H8.inputWrapper-3aw2Sf > input, #app-mount > div.app-1q1i1E > div > div > div > form > div > div > div.mainLoginContainer-1ddwnR > div.block-egJnc0.marginTop20-3TxNs6 > div:nth-child(2) > div > input, #app-mount > div.app-1q1i1E > div > div > div > form > div > div > div.mainLoginContainer-1ddwnR > div.block-egJnc0.marginTop20-3TxNs6 > button.marginBottom8-AtZOdT.button-3k0cO7.button-38aScr.lookFilled-1Gx00P.colorBrand-3pXr91.sizeLarge-1vSeWK.fullWidth-1orjjo.grow-q77ONN');
+  await page.waitForSelector('input[name="email"], input[name="password"], button[type="submit"]');
 
-  await page.type('#app-mount > div.app-1q1i1E > div > div > div > form > div > div > div.mainLoginContainer-1ddwnR > div.block-egJnc0.marginTop20-3TxNs6 > div.marginBottom20-32qID7 > div > div.inputWrapper-31_8H8.inputWrapper-3aw2Sf > input', 'auinheroku@gmail.com');
-  await page.type('#app-mount > div.app-1q1i1E > div > div > div > form > div > div > div.mainLoginContainer-1ddwnR > div.block-egJnc0.marginTop20-3TxNs6 > div:nth-child(2) > div > input', 'auinheroku123!');
-  await page.click('#app-mount > div.app-1q1i1E > div > div > div > form > div > div > div.mainLoginContainer-1ddwnR > div.block-egJnc0.marginTop20-3TxNs6 > button.marginBottom8-AtZOdT.button-3k0cO7.button-38aScr.lookFilled-1Gx00P.colorBrand-3pXr91.sizeLarge-1vSeWK.fullWidth-1orjjo.grow-q77ONN');
+  await page.type('input[name="email"]', 'auinheroku@gmail.com');
+  await page.type('input[name="password"]', 'auinheroku123!');
+  await page.click('button[type="submit"]');
 
-  await page.waitForSelector('#app-mount > div.app-1q1i1E > div > div.leftSplit-1qOwnR.nonEmbeddedLeftSplit-3z6mge > div > div > div.footer-3ZalXG > button.button-38aScr.lookFilled-1Gx00P.colorBrand-3pXr91.sizeMedium-1AC_Sl.grow-q77ONN');
-  await page.click('#app-mount > div.app-1q1i1E > div > div.leftSplit-1qOwnR.nonEmbeddedLeftSplit-3z6mge > div > div > div.footer-3ZalXG > button.button-38aScr.lookFilled-1Gx00P.colorBrand-3pXr91.sizeMedium-1AC_Sl.grow-q77ONN');
+  await page.waitForSelector('button[type="button"]:nth-of-type(2)');
+  await page.click('button[type="button"]:nth-of-type(2)');
   await page.waitForNavigation({ waituntil: 'networkidle0' });
 
   // Now That We Have Authenticated, Make The Tunnel
