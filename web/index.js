171447408c086fb261076d0ac1bc24fa2cd5cf72
TheBotlyNoob <thebotlynoob@gmail.com>
2021-06-17 20:44:56 -0400

    fix website, again

--- web/index.js
+++ web/index.js
@@ -1,7 +1,7 @@
 (async () => {
     var url = await fetch("/url");
 
-    if(!url || !url.ok) document.getElementById("tunnelurl").innerHTML = `The URL Doesn't See, To Exist\n<br/>\nPlease <a href="https://github.com/TheBotlyNoob/Among-Us-Heroku/issues/new?body=logs:\n\`\`\`sh-session\n${await (await fetch('/proxy.log'))}\n\`\`\`&title=URL Doesn't Exist">Make An Issue</a>`;
+    if(!url || !url.ok) document.getElementById("tunnelurl").innerHTML = `The URL Doesn't See, To Exist\n<br/>\nPlease <a href="https://github.com/TheBotlyNoob/Among-Us-Heroku/issues/new?body=logs:\\n\`\`\`sh-session\\n${await (await fetch('/proxy.log')).text()}\\n\`\`\`&title=URL Doesn't Exist">Make An Issue</a>`;
     
     else {
         url = await url.text();
