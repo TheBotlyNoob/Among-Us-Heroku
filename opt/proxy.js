3a8af6533a2032336c484255e02b1a607bffcef3
TheBotlyNoob <thebotlynoob@gmail.com>
2021-07-29 09:07:13 -0400

    update proxy

--- opt/proxy.js
+++ opt/proxy.js
@@ -2,42 +2,52 @@ const spawn = require('child_process').spawn,
   puppeteer = require('puppeteer'),
   { writeFileSync, existsSync } = require('fs');
 
-const proxy = spawn(`${__dirname}/proxy`, { cwd: __dirname });
+const proxy = spawn(`${__dirname}/proxy.exe`, { cwd: __dirname });
 
-if(existsSync(`${__dirname}/../bin/url`)) throw 'Already Claimed The URL!';
+if (existsSync(`${__dirname}/../bin/url`)) throw 'Already Claimed The URL!';
 
-proxy.stderr.on('data', data => data.toString().match(/\bhttps?:\/\/[0-9a-z\/]*/gi) ? claimPlayit(data.toString().match(/https:\/\/[0-9a-z\.\/]*/gi)[0], 'UDP', 22023, `${__dirname}/../bin/url`): '')
-proxy.on('exit', code => {
+proxy.stderr.on('data', (data) =>
+  data.toString().match(/\bhttps?:\/\/[0-9a-z\/]*/gi)
+    ? claimPlayit(
+        data.toString().match(/https:\/\/[0-9a-z\.\/]*/gi)[0],
+        'UDP',
+        22023,
+        `${__dirname}/../bin/url`
+      )
+    : ''
+);
+proxy.on('exit', (code) => {
   console.log(`Proxy Exited With Code: ${code}`);
 });
 
 async function claimPlayit(url, type, port, file) {
-  if(url instanceof String || type instanceof String || port instanceof Number) throw new Error('URL Must Be A String, Type Must Be A String, And The Port Must Be A Number!');
+  if (url instanceof String || type instanceof String || port instanceof Number)
+    throw new Error(
+      'URL Must Be A String, Type Must Be A String, And The Port Must Be A Number!'
+    );
 
-  const browser = await puppeteer.launch({   
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-    ]
-  }),
+  const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }),
     page = await browser.newPage();
 
   // Playit Needs Authentication From Discord
-  await page.goto('https://discord.com/login?redirect_to=%2Foauth2%2Fauthorize%3Fresponse_type%3Dtoken%26client_id%3D705634226527141919%26redirect_uri%3Dhttps%253A%252F%252Fplayit.gg%252Foauth%252Fdiscord%26scope%3Didentify')
-  await page.waitForSelector('input[name="email"], input[name="password"], button[type="submit"]');
+  // await page.waitForSelector(
+  //   'input[name="email"], input[name="password"], button[type="submit"]'
+  // );
 
-  await page.type('input[name="email"]', 'auinheroku@gmail.com');
-  await page.type('input[name="password"]', 'auinheroku123!');
-  await page.click('button[type="submit"]');
+  // await page.type('input[name="email"]', 'auinheroku@gmail.com');
+  // await page.type('input[name="password"]', 'auinheroku123!');
+  // await page.click('button[type="submit"]');
 
-  await page.waitForSelector('button[type="button"]:nth-of-type(2)');
-  await page.click('button[type="button"]:nth-of-type(2)');
-  await page.waitForNavigation({ waituntil: 'networkidle0' });
+  // await page.waitForSelector('button[type="button"]:nth-of-type(2)');
+  // await page.click('button[type="button"]:nth-of-type(2)');
+  // await page.waitForNavigation({ waituntil: 'networkidle0' });
 
-  // Now That We Have Authenticated, Make The Tunnel
-  await page.goto(url, { waituntil: 'networkidle0' });
-  await page.goto(`https://playit.gg/manage/set/custom-${type.toLowerCase()}`);
-  
-  //writeFileSync(file, playitURL);
+  // // Now That We Have Authenticated, Make The Tunnel
+  // await page.goto(url, { waituntil: 'networkidle0' });
+  // await page.goto(`https://playit.gg/manage/set/custom-${type.toLowerCase()}`);
+
+  // writeFileSync(file, playitURL);
 }
--- web/index.js
+++ web/index.js
@@ -1,10 +1,16 @@
 (async () => {
-    var url = await fetch('/url');
+  var url = await fetch('/url');
 
-    if(!url || !url.ok) document.getElementById('tunnelurl').innerHTML = `The URL Doesn't See, To Exist\n<br/>\nPlease <a href='https://github.com/TheBotlyNoob/Among-Us-Heroku/issues/new?body=logs:\\n\`\`\`sh-session\\n${await (await fetch('/proxy.log')).text()}\\n\`\`\`&title=URL Doesn't Exist'>Make An Issue</a>`;
-    
-    else {
-        url = await url.text();
-        document.getElementById('tunnelurl').innerHTML = `<a rel='noopener noreferrer' href='${url}'>${url}</a>`;
-    }
-})()
+  if (!url || !url.ok)
+    document.getElementById(
+      'tunnelurl'
+    ).innerHTML = `The URL Doesn't See, To Exist\n<br/>\nPlease Wait At Least 5 Minutes After Deploying\n<br>\nIf You Have Waited 5 Minutes, Please <a href='https://github.com/TheBotlyNoob/Among-Us-Heroku/issues/new?body=logs:\n\`\`\`sh-session\n${await (
+      await fetch('/proxy.log')
+    ).text()}\\n\`\`\`&title=URL Doesn't Exist'>Make An Issue</a>`;
+  else {
+    url = await url.text();
+    document.getElementById(
+      'tunnelurl'
+    ).innerHTML = `<a rel='noopener noreferrer' href='${url}'>${url}</a>`;
+  }
+})();
