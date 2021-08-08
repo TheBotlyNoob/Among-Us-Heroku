013eaf423b31a0773ed876de8f3f8807d28b08ec
TheBotlyNoob <thebotlynoob@gmail.com>
2021-08-08 16:07:57 -0400

    should work...

--- opt/proxy.js
+++ opt/proxy.js
@@ -1,53 +1,13 @@
-const spawn = require('child_process').spawn,
-  puppeteer = require('puppeteer'),
-  { writeFileSync, existsSync } = require('fs');
+const fs = require('fs');
+const PlayIt = require('playit.gg');
+async () => {
+  const playit = await new PlayIt({
+    email: 'auinheroku@gmail.com',
+    password: 'auinheroku123!'
+  });
 
-const proxy = spawn(`${__dirname}/proxy.exe`, { cwd: __dirname });
-
-if (existsSync(`${__dirname}/../bin/url`)) throw 'Already Claimed The URL!';
-
-proxy.stderr.on('data', (data) =>
-  data.toString().match(/\bhttps?:\/\/[0-9a-z\/]*/gi)
-    ? claimPlayit(
-        data.toString().match(/https:\/\/[0-9a-z\.\/]*/gi)[0],
-        'UDP',
-        22023,
-        `${__dirname}/../bin/url`
-      )
-    : ''
-);
-proxy.on('exit', (code) => {
-  console.log(`Proxy Exited With Code: ${code}`);
-});
-
-async function claimPlayit(url, type, port, file) {
-  if (url instanceof String || type instanceof String || port instanceof Number)
-    throw new Error(
-      'URL Must Be A String, Type Must Be A String, And The Port Must Be A Number!'
-    );
-
-  const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    }),
-    page = await browser.newPage();
-
-  // Playit Needs Authentication From Discord
-  // await page.waitForSelector(
-  //   'input[name="email"], input[name="password"], button[type="submit"]'
-  // );
-
-  // await page.type('input[name="email"]', 'auinheroku@gmail.com');
-  // await page.type('input[name="password"]', 'auinheroku123!');
-  // await page.click('button[type="submit"]');
-
-  // await page.waitForSelector('button[type="button"]:nth-of-type(2)');
-  // await page.click('button[type="button"]:nth-of-type(2)');
-  // await page.waitForNavigation({ waituntil: 'networkidle0' });
-
-  // // Now That We Have Authenticated, Make The Tunnel
-  // await page.goto(url, { waituntil: 'networkidle0' });
-  // await page.goto(`https://playit.gg/manage/set/custom-${type.toLowerCase()}`);
-
-  // writeFileSync(file, playitURL);
-}
+  fs.writeFileSync(
+    `${__dirname}/url`,
+    await playit.createTunnel({ port: 22023, type: 'UDP' }).connect_address
+  );
+};
