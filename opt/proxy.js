ff29c2f13516655bcd1207bbb46772a094169c64
TheBotlyNoob <thebotlynoob@gmail.com>
2021-08-15 15:32:29 -0400

    no more worker

--- opt/proxy.js
+++ opt/proxy.js
@@ -2,12 +2,9 @@ const fs = require('fs');
 const PlayIt = require('playit.gg');
 
 (async () => {
-  const playit = await new PlayIt();
+  const playit = await PlayIt();
 
   const tunnel = await playit.createTunnel({ proto: 'UDP', port: 2023 });
 
-  console.log(tunnel.url);
   fs.writeFileSync(`${__dirname}/url`, tunnel.url);
-  console.log('after');
-  console.log(fs.readFileSync(`${__dirname}/url`, { encoding: 'utf8' }));
 })();
--- web/index.js
+++ web/index.js
@@ -2,11 +2,8 @@
   var url = await fetch('/url');
 
   if (!url || !url.ok)
-    document.getElementById(
-      'tunnelurl'
-    ).innerHTML = `The URL Doesn't See, To Exist\n<br/>\nPlease Wait At Least 5 Minutes After Deploying\n<br>\nIf You Have Waited 5 Minutes, Please <a href='https://github.com/TheBotlyNoob/Among-Us-Heroku/issues/new?body=logs:\n\`\`\`sh-session\n${await (
-      await fetch('/proxy.log')
-    ).text()}\\n\`\`\`&title=URL Doesn't Exist'>Make An Issue</a>`;
+    document.getElementById('tunnelurl').innerHTML =
+      'Please Wait For The URI To Generate...';
   else {
     url = await url.text();
     document.getElementById(
