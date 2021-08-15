bd28e1454bf5f2841e3d7a52e4f71498209487be
TheBotlyNoob <thebotlynoob@gmail.com>
2021-08-15 16:35:47 -0400

    sigh

--- opt/proxy.js
+++ opt/proxy.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const PlayIt = require('playit.gg');
-const fetch = require('node-fetch');
 
 (async () => {
   const playit = await PlayIt();
@@ -8,9 +7,4 @@ const fetch = require('node-fetch');
   const tunnel = await playit.createTunnel({ proto: 'UDP', port: 2023 });
 
   fs.writeFileSync(`${__dirname}/url`, tunnel.url);
-
-  while (true)
-    ((ms) => new Promise((res) => setTimeout(res, ms)))(1800000).then(
-      async () => await fetch('http://127.0.0.1')
-    );
 })();
