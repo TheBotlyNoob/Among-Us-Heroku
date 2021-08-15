b1eac29619e7c3888153c22e93ff2f6ab2d71c69
TheBotlyNoob <thebotlynoob@gmail.com>
2021-08-15 16:14:29 -0400

    add logs to ui

--- web/index.js
+++ web/index.js
@@ -1,6 +1,9 @@
-(async () => {
-  var url = await fetch('/url');
+'use strict';
 
+(async () => {
+  var _await$await$fetch$te;
+
+  var url = await fetch('/url');
   if (!url || !url.ok)
     document.getElementById('tunnelurl').innerHTML =
       'Please Wait For The URI To Generate...';
@@ -10,4 +13,9 @@
       'tunnelurl'
     ).innerHTML = `<a rel='noopener noreferrer' href='${url}'>${url}</a>`;
   }
+  document.getElementById('logs').innerHTML =
+    (_await$await$fetch$te = await (await fetch('/log')).text()) !== null &&
+    _await$await$fetch$te !== void 0
+      ? _await$await$fetch$te
+      : '';
 })();
